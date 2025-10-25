96971a83a0d5a01e08494ab6131f6a463bb41965
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-10-24 19:26:54 -0500

    estilo de sala lectura merge

--- routes/statisticsRoutes.js
+++ routes/statisticsRoutes.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const router = express.Router();
-const {estadisticaLibros,estadisticaUsuarios,getTotal} = require('../controllers/estadisicasController')
+const {estadisticaLibros,estadisticaUsuarios,getTotal, getActiveLoansForUser} = require('../controllers/estadisicasController')
+const verifyToken = require('../middleware/authMiddleware'); 
 
 router.get('/libros',estadisticaLibros);
 
@@ -8,5 +9,6 @@ router.get('/usuarios',estadisticaUsuarios);
 
 router.get('/getTotal',getTotal);
 
+router.get('/prestamos-activos', verifyToken, getActiveLoansForUser);
 
 module.exports = router;
