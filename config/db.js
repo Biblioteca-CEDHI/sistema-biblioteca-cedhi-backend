0f57001129474bb18eadbba90fb62176f5533063
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-11-01 19:29:41 -0500

    db js 3

--- config/db.js
+++ config/db.js
@@ -7,11 +7,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    //port:process.env.DB_PORT,
     dialect: 'postgres',
-    logging: (message) => {
-      if (message.includes('ERROR')) {
-        console.log(message);
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // habilita SSL para Render
+      },
+    },
+    logging: (msg) => {
+      if (msg.includes('ERROR')) {
+        console.log(msg);
       }
     },
   }
